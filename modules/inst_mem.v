cc5d0c1ebeb5feb838edbcc47ce2e1701ffe2a63
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-07 10:05:52 +0530

    Update inst_mem.v

--- modules/inst_mem.v
+++ modules/inst_mem.v
@@ -1,7 +1,23 @@
+/*
+memory to store instructions
+1K x 32
+1 read port,1 write port
+*/
+
 module inst_mem (
-  input write,
-  input addr_w,
-  input addr_r,
-  output read,
+  input clk,
+  input clr,
+  input [31:0] data_in,
+  input [9:0] addr_w,addr_r,
+  output [31:0] data_out,
 )
+  reg [31:0] mem[1023:0] ,out;
+  
+  always@(posedge clk) begin
+    out <= mem[addr_r];
+    mem[addr_w] <= in;
+  end
+  assign data_out = out;
+endmodule
+  
   
