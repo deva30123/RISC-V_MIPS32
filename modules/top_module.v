46c6a8138b8e8b7acea1438c0e23c8c0c9c6adc4
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-08 01:06:47 +0530

    Update top_module.v

--- modules/top_module.v
+++ modules/top_module.v
@@ -1,1 +1,11 @@
-
+/*
+will have 2 modes
+1. code:- will write instructions into the inst mem 
+2 excecute:- will excecute the instructions in the inst bank in repeat until halted
+*/
+module mips32(
+  input mode,
+  input in,
+  input enter,
+  input clr
+)
