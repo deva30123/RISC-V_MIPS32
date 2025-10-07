113577b54e36e4c3359924f15bcbd552fa36ba4f
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-07 16:22:05 +0530

    Update inst_mem.v

--- modules/inst_mem.v
+++ modules/inst_mem.v
@@ -1,3 +1,4 @@
+// Code your design here
 /*
 memory to store instructions
 1K x 32
@@ -6,19 +7,19 @@ memory to store instructions
 
 module inst_mem (
   input clk,
-  input clr,
+  
   input [31:0] data_in,
   input [9:0] addr_w,addr_r,
-  output [31:0] data_out,
-)
-  reg [31:0] mem[1023:0] ,out;
+  output [31:0] data_out
+);
+  reg [31:0] mem [1023:0] ;
+  reg [31:0] out;
   
   always@(posedge clk) begin
-    if (clr==1) mem = {32768{1'b0}};
-    else begin
+    
       out <= mem[addr_r];
-      mem[addr_w] <= in;
-    end
+      mem[addr_w] <= data_in;
+    
   end
   assign data_out = out;
 endmodule
