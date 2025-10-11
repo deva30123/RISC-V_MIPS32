235e6b937bbd7b8356a8652006c13b39ce07158a
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-11 18:05:02 +0530

    Update excecute.v

--- modules/EXE/excecute.v
+++ modules/EXE/excecute.v
@@ -10,13 +10,31 @@ module exe(
   output[31:0] B_ex
 );
   wire[31:0] a,b;
-  wire[5:0] opcode;
+  wire[5:0] opcode; 
   reg cond;
   assign opcode = IR[31:26];
   assign a=(opcode[5:2]==4'b1101)?NPC_id:A;
   assign b=(opcode[4])?Imm:B;
   
   always@(*) begin// Alu block
+    if(~opcode[5])begin
+      case(opcode[3:0])
+        4'd0: ALU_out = a+b;
+        4'd1: ALU_out = a-b;
+        4'd2: ALU_out = a*b;
+        4'd3: ALU_out = a>b?1:0;
+        4'd4: ALU_out = a|b;
+        4'd5: ALU_out = a&b;
+      endcase
+    end            // Arithmetic and logic operations
+    else begin
+      ALU_out = a+b; //ld, st, branch all use npc+imm
+    end           //controls
   end
   assign NPC_ex=(cond?)ALU_out:NPC_id;
 endmodule
+/*
+Arithmetic and logic operations {opcode[5] == 0}
+opcode[4] == 0 => RR addressing
+
+*/
