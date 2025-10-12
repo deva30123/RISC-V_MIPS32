5a7286e2fa210517a7ae801e0f6e0bcd69c4fb05
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-12 12:05:15 +0530

    Update memax.v

--- modules/datamem/memax.v
+++ modules/datamem/memax.v
@@ -4,12 +4,12 @@ module memax(
 );
   wire opcode[5:0];
   reg [31:0] data[1023:0];
-  reg LD
+  reg LData;
   opcode[5:0] = IR_ex[31:26];
   always@(*) begin
     if (opcode[5:1]==2'b11000) begin
       case(opcode[0])
-        1'b0:LD=data[ALU_out_ex];    // load
+        1'b0:LData=data[ALU_out_ex];    // load
         1'b0:data[ALU_out_ex]=D_ex;   //store
       endcase
     end
