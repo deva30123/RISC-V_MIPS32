426de153bdab9ad17235006807e26d968e9ac51d
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-12 19:12:36 +0530

    Update excecute.v

--- modules/EXE/excecute.v
+++ modules/EXE/excecute.v
@@ -1,3 +1,4 @@
+// Code your design here
 module exe(
   input [31:0] A,
   input [31:0] B,
@@ -6,13 +7,14 @@ module exe(
   input[31:0] IR_id,
   output [31:0] NPC_ex,
   output[31:0] IR_ex,
-  output[31:0] ALU_out,
+  output[31:0] ALU_res,
   output[31:0] B_ex
 );
   wire[31:0] a,b;
+  reg [31:0] ALU_out;
   wire[5:0] opcode; 
   reg cond;
-  assign opcode = IR[31:26];
+  assign opcode = IR_id[31:26];
   assign a=(opcode[5:2]==4'b1101)?NPC_id:A;
   assign b=(opcode[4])?Imm:B;
   
@@ -32,12 +34,13 @@ module exe(
     end           //controls
   end
   always@(*) begin
-    if(opcode[5:0]==11010)begin
+    if(opcode[5:1]==5'b11010)begin
       cond = opcode[0]^((A==0)?1:0); //  branch
     end
     else cond = 0;
   end
-  assign NPC_ex=(cond?)ALU_out:NPC_id;
+  assign NPC_ex = (cond)?ALU_out:NPC_id;
+  assign ALU_res = ALU_out;
 endmodule
 /*
 Arithmetic and logic operations {opcode[5] == 0}
