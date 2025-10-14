f1bb053f6a45c91d4e91eabfd7ae524e0e2f9ab1
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-14 21:33:15 +0530

    Update ifetch.v

--- modules/fetch/ifetch.v
+++ modules/fetch/ifetch.v
@@ -1,16 +1,19 @@
 //`include "mem.v"
 module ifetch(
-  input clk,
-  input [31:0]ALU,
-  input sel,
-  input [31:0]NPC_mem,
+  input clk,   
+  input [31:0]NPC_alu,
   output [31:0]NPC,
   output [31:0]IR
 );
   reg [31:0] mem [1023:0] ;
   reg [31:0] PC;
-  reg [31:0]IR;
-  assign NPC = sel?ALU:(PC+1);  
+  reg [31:0]Ir;
+  always@(posedge clk)begin
+    PC = NPC_alu;
+    Ir = mem[PC];
+  end
+  assign IR = Ir;
+  assign NPC = PC+1;  
 endmodule
 /*
 Ext mem block not incorporated
