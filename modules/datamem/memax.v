53f96bf81358043c139bcf9491c06f47d9828827
deva30123 <devajothi.sardar3120@gmail.com>
2025-10-15 00:27:25 +0530

    Update memax.v

--- modules/datamem/memax.v
+++ modules/datamem/memax.v
@@ -1,13 +1,13 @@
 module memax(
-  input [31:0] IR_ex, ALU_out_ex, D_ex,
-  output [31:0] IR_mem, LMD, ALU_out_mem
+  input [31:0] IR_ex, ALU_ex, D_ex,
+  output [31:0] IR_mem, LMD, ALU_mem
 );
   wire opcode[5:0];
   reg [31:0] data[1023:0];
   //reg LData;
   opcode[5:0] = IR_ex[31:26];
   always@(*) begin
-    data[ALU_out_ex]=(opcode==6'b110001)?D_ex:data[ALU_out_ex];
+    data[ALU_ex]=(opcode==6'b110001)?D_ex:data[ALU_ex];
     // if (opcode[5:1]==2'b11000) begin
     //   case(opcode[0])
     //     1'b0:LData=data[ALU_out_ex];    // load
@@ -17,6 +17,6 @@ module memax(
     
   end
   assign IR_ex = IR_mem;
-  assign ALU_out_ex = ALU_out_mem;
+  assign ALU_ex = ALU_mem;
   assign LMD = data[ALU_out];
 endmodule
